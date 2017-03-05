c34db9cad90e05c79dfa19c100919f139f1d2714
Mac <Mac@Mac-PC>
2017-03-05 13:20:00 +0800

    201703050119

---
+++ fsit03_java/src/tw/org/iii/Hello.java
@@ -0,0 +1,8 @@
+package tw.org.iii;
+	
+public class Hello {
+	public static void main(String[] ed){
+		System.out.println("Hello, World !");
+
+	}
+}
---
+++ fsit03_java/src/tw/org/iii/ed01.java
@@ -0,0 +1,10 @@
+package tw.org.iii;
+
+public class ed01 {
+
+	public static void main(String[] args) {
+		// TODO «Ý¿ì¨Æ¶µ
+		// FIXME bug«Ý­×
+		// XXX ....
+	}
+}
