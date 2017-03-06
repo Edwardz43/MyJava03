0f089bf3fc3b64dcd99e108eab70f060b6cc4017
EdLo <EdLo@EdLo-PC>
2017-03-07 00:52:52 +0800

    201703070053

--- fsit03_java/src/tw/org/iii/Ed08.java
+++ fsit03_java/src/tw/org/iii/Ed08.java
@@ -9,12 +9,14 @@ public class Ed08 {
         int i =1;
         //建立一個52格的陣列
 
+        
         for(i = 0; i < N; i++) 
         {
             poker[i] = i + 1;// +1 =>位移 
         }
         //將陣列依序產生1~52的數字
 
+        
         for(i = 0; i < N; i++) 
         { 
             int j = (int) (Math.random() * N + 1);
@@ -22,22 +24,25 @@ public class Ed08 {
             poker[i] = poker[j]; 
             poker[j] = tmp; 
         } 
-        // 重點:隨機產生1~52的亂數 弄成陣列J  並將之
-        String [] symbols = {"黑桃", "紅心", "方塊", "梅花"};
+        // 重點:隨機產生1~52的亂數 弄成陣列J  並利用tmp將之替換成陣列poker
+        
+        
+        String [] symbols = {"黑桃", "紅心", "方塊", "梅花"};//做一個花色陣列
         
         for(i = 0; i < N; i++) 
         { 
-            System.out.print(symbols[(poker[i] - 1) / 13]);
+            System.out.print(symbols[(poker[i] - 1) / 13]);//叫出poker 並決定花色
 
             int remain = poker[i] % 13; 
+            
             switch(remain) 
             {  
                 case  0: System.out.print(" K"); break; 
                 case 12: System.out.print(" Q"); break; 
-                case 11: System.out.print(" J"); break; 
+                case 11: System.out.print(" J"); break;     //決定點數
                 default: System.out.printf("%2d", remain); 
             } 
-            System.out.printf("%c", (i + 1) % 13 == 0 ? '\n' : ' '); 
+            System.out.printf("%c", (i + 1) % 13 == 0 ? '\n' : ' '); //補充  每發13張就換行 
         } 
     }
 }
