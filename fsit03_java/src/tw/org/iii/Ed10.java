d7a35fb167323e29e17a3e562df012bf88682626
EdLo <EdLo@EdLo-PC>
2017-03-08 00:52:21 +0800

    201703080052

--- fsit03_java/src/tw/org/iii/Ed10.java
+++ fsit03_java/src/tw/org/iii/Ed10.java
@@ -3,8 +3,8 @@ package tw.org.iii;
 public class Ed10 {
 
 	public static void main(String[] args) {
-		int [] ct = new int []{0,0,0,0,3,0,0,0,0};
-		int [] isl = new int []{4,0,0,2,0,0,5,0,0};
+		int [] ct = new int []{0,0,0,0,3,0,0,4,0};
+		int [] isl = new int []{4,0,0,2,0,0,5,4,0};
 		System.out.printf("CT" + "\t");
 		for(int i=0;i<9;i++){
 			System.out.printf("%2d",ct[i]);	
