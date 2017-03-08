f36b67946404e96348db4719962445a7fcb25f22
EdLo <b93303015@gmail.com>
2017-03-08 22:37:44 +0800

    201703082238

--- fsit03_java/src/tw/org/iii/Ed11.java
+++ fsit03_java/src/tw/org/iii/Ed11.java
@@ -3,36 +3,40 @@ package tw.org.iii;
 import java.util.Scanner;
 
 public class Ed11 {
-	
-	private static String draw(){
-		System.out.print("*");	
-		return "";
-	}
-	//draw方法  畫星星
-	
-	public static void main(String[] args) {
-		System.out.print("輸入大小:");
-		Scanner scan = new Scanner(System.in);
-		int input = scan.nextInt() + 1;
-		//決定圖形的大小 每邊有幾個*
 		
+	private static int draw(int i){	
+		return i;
+	}
+	//draw方法  畫星星  目前改成畫數字
+
+	
+	public static void loop(int input){	//改造成loop方法  練習一下
 		for(int i = 1; i < input; i++){
-			System.out.printf(String.format("%%%ds", (input - i) * 3), "");//這段不太了解  是看書上的範例  
-			for(int j = 0; j < i; j++){									   //主要是算出每行前面要空幾格讓形狀對齊
-				System.out.printf("%5s", draw());//呼叫draw方法  畫星星  %5s讓每行的星星間隔一樣
+			System.out.printf(String.format("%%%ds", (input - i) * 2), "");//這段不太了解  是看書上的範例  
+			for(int j = 0; j < i; j++){//主要是算出每行前面要空幾格讓形狀對齊
+				System.out.printf("%4s", draw(j+1));//呼叫draw方法  畫星星  %5s讓每行的星星間隔一樣
 			}	
 			System.out.println();	
 		}
 		//畫出1~N層
 		
-		
 		for(int i = input - 2; i > 0; i--){              //反過來畫 所以迴圈的順序想了一下 讓他顛倒
-			System.out.printf(String.format("%%%ds", (input - i) * 3), "");
+			System.out.printf(String.format("%%%ds", (input - i) * 2), "");
 			for(int j = 0; j < i; j++){
-				System.out.printf("%5s", draw());
+				System.out.printf("%4s", draw(j+1));
 			}	
 			System.out.println();	
 		}
-		//畫出反方向的(N-1)~1層
-	}	
+		//畫出反方向的(N-1)~1層	
+	}
+	
+	
+	public static void main(String[] args) {   //主要方法  
+		System.out.print("輸入大小:");
+		Scanner scan = new Scanner(System.in);
+		int INPUT = scan.nextInt() + 1;
+		//決定圖形的大小 每邊有幾個*
+		loop(INPUT);
+	}
+	
 }
