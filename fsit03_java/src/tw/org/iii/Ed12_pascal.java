820e6b14fd2b35c4ff3bec1bd9cccc270aba9d94
EdLo <b93303015@gmail.com>
2017-03-10 02:13:20 +0800

    201703100213

--- fsit03_java/src/tw/org/iii/Ed12_pascal.java
+++ fsit03_java/src/tw/org/iii/Ed12_pascal.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 
 public class Ed12_pascal {
 	
-	private static int comb(int r, int n){
+	private static int comb(int r, int n){  //數學公式  C(組合): r中取n的方法 巴斯卡三角的公式  先不管他
 		int p = 1;
 		for(int i = 0; i <= n; i++){
-			if(i == 0){
+			if(i == 0){  //解決尾巴的1  加個判斷
 				p = 1;
 			}else{
 			p = p * (r- i + 1) / i;
@@ -15,12 +15,12 @@ public class Ed12_pascal {
 		}return p;
 	}
 	
-	public static void loop(int input){	//改造成loop方法  練習一下
-		System.out.printf(String.format("%%%ds", (input) * 2 + 5), "1" + "\n");
+	public static void loop(int input){	
+		System.out.printf(String.format("%%%ds", (input) * 2 + 5), "1" + "\n");//先印出第一行的1 後面會一個一樣的 印出最後一個1
 		for(int i = 1; i < input; i++){
-			System.out.printf(String.format("%%%ds", (input - i) * 2), "");//這段不太了解  是看書上的範例  
-			for(int j = 0; j <= i; j++){//主要是算出每行前面要空幾格讓形狀對齊
-				System.out.printf("%4s", comb(i,j));//呼叫draw方法  畫星星  %5s讓每行的星星間隔一樣
+			System.out.printf(String.format("%%%ds", (input - i) * 2), "");  
+			for(int j = 0; j <= i; j++){
+				System.out.printf("%4s", comb(i,j));
 			}	
 			System.out.println();	
 		}
@@ -36,11 +36,13 @@ public class Ed12_pascal {
 		System.out.printf(String.format("%%%ds", (input) * 2 + 5), "1" + "\n");
 		//畫出反方向的(N-1)~1層	
 	}
+	
+	
 	public static void main(String[] args) {   //主要方法  
 		System.out.print("輸入大小:");
 		Scanner scan = new Scanner(System.in);
 		int INPUT = scan.nextInt() + 1;
-		//決定圖形的大小 每邊有幾個*
+		//決定圖形的大小 
 		loop(INPUT);
 	}
 
