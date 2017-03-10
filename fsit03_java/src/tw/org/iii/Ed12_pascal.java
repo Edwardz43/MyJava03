6c6c33ddc211d9dcd8978d94c92a886f2bae5b63
EdLo <b93303015@gmail.com>
2017-03-10 19:15:00 +0800

    201703101914

--- fsit03_java/src/tw/org/iii/Ed12_pascal.java
+++ fsit03_java/src/tw/org/iii/Ed12_pascal.java
@@ -6,8 +6,8 @@ public class Ed12_pascal {
 	
 	private static int comb(int r, int n){  //數學公式  C(組合): r中取n的方法 巴斯卡三角的公式  先不管他
 		int p = 0;
-		for(int i = 0; i <= n; i++){
-			if(i == 0){  //解決尾巴的1  加個判斷
+		for(int i = 0; i <= n; i++){  ////解決第一行和尾巴的1  加個判斷  
+			if(i == 0 || r ==0){  
 				p = 1;
 			}else{
 			p = p * (r- i + 1) / i;
@@ -16,25 +16,22 @@ public class Ed12_pascal {
 	}
 	
 	public static void loop(int input){	
-		System.out.printf(String.format("%%%ds", (input + 1) * 2 + 5), "1" + "\n");//先印出第一行的1 後面會一個一樣的 印出最後一個1
-		for(int i = 1; i <= input; i++){
-			System.out.printf(String.format("%%%ds", (input - i + 1) * 2), "");  
+		for(int i = 0; i <= input; i++){
+			System.out.printf(String.format("%%%ds", (input - i + 1) * 3), "");  
 			for(int j = 0; j <= i; j++){
-				System.out.printf("%4s", comb(i,j));
+				System.out.printf("%6s", comb(i,j));
 			}	
 			System.out.println();	
 		}
 		//畫出1~N層
 		
-		for(int i = input - 1; i > 0; i--){              //反過來畫 所以迴圈的順序想了一下 讓他顛倒
-			System.out.printf(String.format("%%%ds", (input - i + 1) * 2), "");
+		for(int i = input - 1; i >= 0; i--){              //反過來畫 所以迴圈的順序想了一下 讓他顛倒
+			System.out.printf(String.format("%%%ds", (input - i + 1) * 3), "");
 			for(int j = 0; j <= i; j++){
-				System.out.printf("%4s", comb(i, j));
+				System.out.printf("%6s", comb(i, j));
 			}	
 			System.out.println();	
 		}
-		System.out.printf(String.format("%%%ds", (input +1) * 2 + 5), "1" + "\n");
-		//畫出反方向的(N-1)~1層	
 	}
 	
 	
