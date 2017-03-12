c103049c0481b03077710ab60b4666741f3ac31b
EdLo <b93303015@gmail.com>
2017-03-12 22:42:55 +0800

    201703122242

--- fsit03_java/src/tw/org/iii/Class311_05.java
+++ fsit03_java/src/tw/org/iii/Class311_05.java
@@ -2,24 +2,28 @@ package tw.org.iii;
 
 public class Class311_05 {
 	
-	static boolean getPrime(int input){
-		int[] prime = new int[]{2,3,5,7};
-		for(int i = 0; i < prime.length; i++){
-			if(input == prime[i]){
+	static boolean Prime(int input){
+			int sum = 0;
+			for(int i=1;i<=input;i++){
+				if(input%i ==0){
+					sum++;
+				}
+			}
+			if(sum ==2){
 				return true;
-			}else if (input % prime[i]==0){
-				return false;
 			}else{
-				return true;
+				return false;
 			}
 		}
-		return true;
-	}
+		
+		
+	
+	
 	public static void main(String[] args) {		
 		for(int j = 0; j < 10; j++ ){         //開始處理11~100
 			for(int i = 1; i <= 10; i ++){
 				int newi = i + j *10;		
-				System.out.print(getPrime(newi)?newi+"*":newi+"");
+				System.out.print(Prime(newi)?newi+"*"+"\t":newi+"\t");
 			}
 		System.out.println();
 		}
