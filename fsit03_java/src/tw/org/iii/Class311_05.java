476101205164899e9049cdc0be6457186ac9e46d
Mac <Mac@Mac-PC>
2017-03-12 17:16:55 +0800

    201703121716

--- fsit03_java/src/tw/org/iii/Class311_05.java
+++ fsit03_java/src/tw/org/iii/Class311_05.java
@@ -1,33 +1,27 @@
 package tw.org.iii;
 
 public class Class311_05 {
-	public static void main(String[] args) {
-		int[] prime = new int[]{2,3,5,7};
-		String[] primeFirst = new String[]{"1","2*","3*","4","5*","6","7*","8","9","10"};//第一行的質數很難處理 所以選擇直接手動印出
-		for(int i = 0; i <primeFirst.length; i ++){
-			System.out.print(primeFirst[i]+"\t");
-		}
-		System.out.println();
-		
-		for(int j = 1; j < 10; j++ ){         //開始處理11~100
-			for(int i = 1; i <= 10; i ++){
-				int newi = i + j *10;
-				boolean isPrime = true;       //撲克牌的類似應用  判斷是否質數
-				for(int k=0; k<prime.length; k++){
-					if(newi % prime[k] == 0){
-						isPrime = false;
-						break;
-					}					
-				}
-				if(!isPrime){                 //用if判斷 是質數就+*號
-					System.out.print(newi+"\t");
-				}else{
-					System.out.print(newi+"*"+"\t");
-				}
-			}
-			System.out.println();
-		}	
-	}
 	
-
+	static boolean getPrime(int input){
+		int[] prime = new int[]{2,3,5,7};
+		for(int i = 0; i < prime.length; i++){
+			if(input == prime[i]){
+				return true;
+			}else if (input % prime[i]==0){
+				return false;
+			}else{
+				return true;
+			}
+		}
+		return true;
+	}
+	public static void main(String[] args) {		
+		for(int j = 0; j < 10; j++ ){         //開始處理11~100
+			for(int i = 1; i <= 10; i ++){
+				int newi = i + j *10;		
+				System.out.print(getPrime(newi)?newi+"*":newi+"");
+			}
+		System.out.println();
+		}
+	}	
 }
