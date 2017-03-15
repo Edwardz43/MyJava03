223373606cbe4b3b020b240932934ece35bea09c
EdLo <b93303015@gmail.com>
2017-03-15 15:27:17 +0800

    201703151527

--- fsit03_java/src/tw/org/iii/Ed17_calender.java
+++ fsit03_java/src/tw/org/iii/Ed17_calender.java
@@ -14,6 +14,8 @@ public class Ed17_calender {
 	 *  y:年分後兩位(1,2月y-1)
 	 *  m:月份-2 (1,2月 =>11,12)
 	 *  d:日期
+	 *  
+	 *  3/15 新增方法 setDays 主要是想讓main方法簡單明瞭
 	*/
 	
 	//1. 利用公式找出每月的第一天
@@ -33,7 +35,28 @@ public class Ed17_calender {
 		return W;
 	}
 
-	//2. 判斷平/閏年
+	//2. 決定天數  我用swith + 一個if來判斷2月
+	static int setDays(int y ,int m){
+		int d = 0;
+		switch(m){
+		case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+			d = 31;
+			break;
+		case 4: case 6: case 9: case 11:
+			d = 30;
+			break;
+		//2.1 判斷平/閏年
+		case 2:
+			if(learyear(y)){
+				d = 29; 
+			}else{
+				d = 28;
+			}
+		}
+		return d;
+	}
+	
+	//2.1 判斷平/閏年
 	//禮拜六上的內容  直接COPY來用吧!
 	static boolean learyear(int year){
 		boolean learyear = true;
@@ -51,8 +74,7 @@ public class Ed17_calender {
 	    return false;
 	}
 	
-	//3. 產生月曆 
-	// 最麻煩的來了...
+	//3. 產生月曆   最麻煩的來了...
 	static void result(int w, int d){  //新增一個 result方法   輸入每個月的第一天是禮拜幾(w)以及當月一共有幾天(d)
 		//3.1 弄一個陣列  根據你輸入的天數(d) 產生相同數量的陣列  然後for一次把數字(i)塞入
 		int[] day = new int[d];
@@ -72,9 +94,9 @@ public class Ed17_calender {
 		System.out.println();
 		
 		//3.4 第二列以後的就會按照規律  所以寫法跟質數的作業類似   不過要記得扣掉第一列的那幾天
-		// 3/15 修改一下輸出的樣式  為了整齊美觀
-		for(int i=0;i<day.length-7+w;i++){
-			System.out.print((day[i+1]+7-w)<10?"  "+(day[i+1]+7-w)+"\t":" "+(day[i+1]+7-w)+"\t");//輸出天數時要多"+1"
+		// 3/15 修改一下輸出的樣式  為了整齊美觀  1~9號輸出時前面空兩格  其他的空一格
+ 		for(int i=0;i<day.length-7+w;i++){
+			System.out.print((day[i]+8-w)<10?"  "+(day[i]+8-w)+"\t":" "+(day[i]+8-w)+"\t");//輸出天數時要多"+1"
 			if(i%7 ==6){
 				System.out.println();
 			}
@@ -82,6 +104,7 @@ public class Ed17_calender {
 	}
 	
 	
+	// 經過修改  main方法縮短很多   增加閱讀性
 	public static void main(String[] args) {
 		// 3/15 修改輸入模式   年分和月分開  更清楚   介面改成 JOP
 		String Y = JOptionPane.showInputDialog("請輸入年分:\n(格式:yyyy)");
@@ -91,28 +114,12 @@ public class Ed17_calender {
 		
 		//1. 利用公式找出每月的第一天
 		int w = week(year,month);
-		int d = 0;
 		
-		//決定天數  我用swith + 一個if來判斷2月
-		switch(month){
-		case 1: case 3: case 5: case 7: case 8: case 10: case 12:
-			d =31;
-			break;
-		case 4: case 6: case 9: case 11:
-			d = 30;
-			break;
 		
-		//2. 判斷平/閏年
-		case 2:
-			if(learyear(year)){
-				d = 29; 
-			}else{
-				d = 28;
-			}
-		    break;
-		}
+		//2. 決定天數
+		int d =setDays(year,month);
 		
-		// 預先列出格式
+		// 預先列出layout
 		String[] week = {"Sun","Mon","Tue","Wed","Thu","Fri","Sat"};
 		for(String v :week){
 			System.out.print(v+"\t");
